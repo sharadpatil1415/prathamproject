e271d06e29923f1c35d184c1e18a7c1df28cb810
aaryakhedekar <khedekaraarya36@gmail.com>
2024-02-03 00:27:16 +0530

    commit3

--- User/JavaScript/POs.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('.program-indi').addEventListener('click', openPopup);
-
-    function openPopup() {
-    document.getElementById('popupContainer').style.display = 'flex';
-    }
-
-    function closePopup() {
-    document.getElementById('popupContainer').style.display = 'none';
-    }
